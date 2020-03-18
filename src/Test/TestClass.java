097d735259c482fa379d1dc5c95997ec9dcc944f
abdagnachew <abdagnachew@gmail.com>
2020-03-17 20:37:15 -0400

    Message out

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -3,8 +3,9 @@ package Test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+System.out.println("Hello");
 
+System.out.println("2nd change");
 	}
 
 }
